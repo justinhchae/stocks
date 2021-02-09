1aa0c625aa2ff53a03b91e84422feeb6854d0b04
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-02-08 22:01:31 -0500

    make TODO list

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ reddits = GetReddit()
 reddits.get_threads()
 
 
+#TODO get reddit and or twitter data for at least 1-2020 to 1-2021, more is better, if possible.
+
+#TODO make cleaner module (for social media data)
+
+#TODO make sentiment analysis module (for social media data)
+
+#TODO prepare stock data for time series analysis
+
+#TOD prepare nueral network for sentiment/stock price learning
