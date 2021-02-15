c725bcb23b800271b580e08b74c3f46cdeafc92d
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-02-15 15:31:09 -0500

    Update lstm_approach_1.py

--- model/lstm_approach_1.py
+++ model/lstm_approach_1.py
@@ -9,9 +9,16 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+sequence_length = 59
 
-def make_sequence(data, data_col, seq_len=59):
+
+def make_sequence(data, data_col, seq_len=sequence_length):
+    """
+    return sequenced stock data as torch tensors
+    of shape len(data) by sequence length
+    """
     x, y = [], []
+
     arr = data[data_col]
 
     for i in range(len(arr) - seq_len):
@@ -29,6 +36,8 @@ def make_sequence(data, data_col, seq_len=59):
 
     return x_var, y_var
 
+
+
 def prep_arr(df, time_col, data_col):
     data_dict = {}
 
@@ -40,105 +49,66 @@ def prep_arr(df, time_col, data_col):
 
     return data_dict
 
+
 class Model(nn.Module):
-    # https://stackabuse.com/time-series-prediction-using-lstm-with-pytorch-in-python/
-    def __init__(self, input_size=1, hidden_layer_size=59, output_size=1, device="cpu"):
-        super().__init__()
 
-        if device == "gpu" and torch.cuda.is_available():
-            self.device = torch.device("cuda")
-        else:
-            self.device =  torch.device("cpu")
+    def __init__(self
+                 , input_dim=1
+                 , hidden_dim=sequence_length
+                 , output_dim=1
+                 # , num_layers=2
+                 ):
 
-        self.hidden_layer_size = hidden_layer_size
+        super(Model, self).__init__()
 
-        self.lstm = nn.LSTM(input_size, hidden_layer_size)
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        # self.num_layers = num_layers
 
-        self.linear = nn.Linear(hidden_layer_size, output_size)
+        # Define the LSTM layer
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim)
 
-        self.hidden_cell = (torch.zeros(1,1,self.hidden_layer_size, device=self.device),
-                            torch.zeros(1,1,self.hidden_layer_size, device=self.device))
+        # Define the output layer
+        self.linear = nn.Linear(self.hidden_dim, output_dim)
+
+    def init_hidden(self):
+        # This is what we'll initialise our hidden state as
+        return (torch.zeros(1,1, self.hidden_dim),
+                torch.zeros(1,1, self.hidden_dim))
+
+    def forward(self, input):
+        # Forward pass through LSTM layer
+        # shape of lstm_out: [input_size, batch_size, hidden_dim]
+        # shape of self.hidden: (a, b), where a and b both
+        # have shape (num_layers, batch_size, hidden_dim).
+        # lstm_out, self.hidden = self.lstm(input.view(len(input), self.batch_size, -1))
+        lstm_out, self.hidden = self.lstm(input.view(len(input), 1, -1), self.hidden)
+
+        # Only take the output from the final timetep
+        # Can pass on the entirety of lstm_out to the next layer if it is a seq2seq prediction
+        # y_pred = self.linear(lstm_out[-1].view(self.batch_size, -1))
+        # return y_pred.view(-1)
+
+        predictions = self.linear(lstm_out.view(len(input), -1))
+        print(predictions)
+        return predictions[-1]
 
 
-    def forward(self, input_seq):
-        lstm_out, self.hidden_cell = self.lstm(input_seq.view(len(input_seq) ,1, -1), self.hidden_cell)
-        # predictions = self.linear(lstm_out.view(len(input_seq), -1))
-        # return predictions[-1]
-        return lstm_out, self.hidden_cell
+def train_model_1(df, epochs=3, learning_rate=0.01):
 
-
-def train_model_1(df, epochs=150):
     data = prep_arr(df, time_col='t', data_col='c')
 
     x_train, y_train = make_sequence(data=data, data_col='c')
 
-
     model = Model()
-    model= model.to(model.device)
-    model.train()
 
-    loss_function = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    loss_function = nn.MSELoss(size_average=False)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-    x_train = x_train.to(model.device)
-    y_train = y_train.to(model.device)
+    # start loop step 1
+    model.zero_grad()
 
-    for i in range(epochs):
-        losses = []
-        optimizer.zero_grad()
+    model.hidden = model.init_hidden()
 
-        for idx, input in enumerate(x_train):
-
-            # model.hidden_cell = (torch.zeros(1, 1, model.hidden_layer_size, device=model.device),
-            #                      torch.zeros(1, 1, model.hidden_layer_size, device=model.device))
-
-            # print('input', input[-3:])
-            # print('target', y_train[idx][:3])
-            output, state = model(input)
-            model.hidden_cell = state
-            predictions = model.linear(output.view(len(input), -1))
-            y_pred = predictions[-1]
-            # print('pred:', y_pred)
-
-            y = torch.tensor([y_train[idx][0]])
-
-            loss = loss_function(y_pred, y)
-            # print('loss', loss)
-
-            losses.append(loss.item())
-
-            loss.backward(retain_graph=True)
-
-            if idx == 100:
-                break
-        optimizer.step()
-
-        plt.figure()
-        plt.plot(losses)
-        plt.show()
-        break
-
-        # complete a single sequence
-
-        # if i%25 ==1:
-        #     print(f'epoch: {i:3} loss: {loss.item():10.8f}')
-        #
-        # print(loss)
-
-
-
-
-# def predict_model_1():
-#     model.eval()
-#
-#     for i in range(fut_pred):
-#         seq = torch.FloatTensor(test_inputs[-train_window:])
-#         with torch.no_grad():
-#             model.hidden = (torch.zeros(1, 1, model.hidden_layer_size),
-#                             torch.zeros(1, 1, model.hidden_layer_size))
-#             test_inputs.append(model(seq).item())
-
-
-
-
-
+    y_pred = model(x_train[0])
+    print(y_pred)
