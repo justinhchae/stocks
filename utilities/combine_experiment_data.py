bug fix - sentiment variance

utilities/combine_experiment_data.py
@@ -29,7 +29,7 @@ def combine_news_stock(stock_df
     df = pd.merge(stock_df, news_df, how='left', left_on=time_col, right_on=time_col)
 
     # get variance of sentiment data to understand more
-    sentiment_variance = np.var(df[sentiment_col].values)
+    # sentiment_variance = np.var(df[sentiment_col].values)
 
 
     # resample to fill in missing values with spline
@@ -84,4 +84,4 @@ def combine_news_stock(stock_df
     fig.savefig(f'figures/{ticker}_data_prep.png')
     # plt.show()
 
-    return df, sentiment_variance
+    return df#, sentiment_variance

utilities/sentiment_data.py
@@ -31,7 +31,7 @@ def score_sentiment(df
     df = df[[date_col, score_type]]
     df = df.set_index(date_col)
 
-    sentiment_variance = np.var(df[score_type].values)
+    # sentiment_variance = np.var(df[score_type].values)
 
     if is_resample:
         df = df.resample('1min').fillna('nearest')
@@ -51,5 +51,5 @@ def score_sentiment(df
 
     df.rename(columns={date_col:'t'}, inplace=True)
 
-    return df, sentiment_variance
+    return df
 