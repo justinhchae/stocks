67340eb1f322422015916e15be9941a98501635d
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-03-07 00:12:58 -0500

    Update debug_mode.py

--- application/debug_mode.py
+++ application/debug_mode.py
@@ -2,10 +2,10 @@ from application.config import *
 
 def debug_mode():
     # set available modes for data
-    pick_list_values = [False, True]
+    pick_list_values = [True, False]
     # convenience object
     sidebar_picklist = pick_list_values
     # return the select box with options as st object
-    return st.sidebar.selectbox(label='Set Debug Mode (Default to False)'
+    return st.sidebar.selectbox(label='Set Debug Mode (Default to True)'
                                 , options=sidebar_picklist
                                 , key='debug_mode')
