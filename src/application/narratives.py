8f0f06a09cc3eaf4151e6c6644eebe632eb4645a
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-03-12 23:01:27 -0500

    Update narratives.py

--- src/application/narratives.py
+++ src/application/narratives.py
@@ -2,8 +2,7 @@ from application.config import *
 
 def narrative_introduction():
     st.header('Concept')
-    st.write(
-        '1. Train various models on stock data with a focus on timeseries and sentiment data from news about the stock.')
+    st.write('1. Train various models on stock data with a focus on timeseries and sentiment data from news about the stock.')
     st.write('2. Baseline forecasting with ARIMA, Facebook Prophet, and LSTM.')
     st.write('3. Combine prices with sentiment scores and train a second LSTM having sentiment scores.')
     st.write('4. Compare results with MAPE (Mean Absolute Percentage Error)')
@@ -15,6 +14,5 @@ def narrative_conclusion():
     st.write('3. LSTMs do okay, both with price and with sentiment, but underperform overall.')
     st.write('4. High levels of variance in sentiment and in price have major impacts on model accuracy.')
     st.write('5. Combination of a forecasting model with a reinforcement learning agent may be interesting.')
-    st.write(
-        '6. Despite lower accuracy for LSTMs, they still tracked the direction of movement and only missed the magnitude.')
+    st.write('6. Despite lower accuracy for LSTMs, they still tracked the direction of movement and only missed the magnitude.')
     st.write(' ')
